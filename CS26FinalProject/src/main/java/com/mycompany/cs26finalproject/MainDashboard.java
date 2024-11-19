b1c8eaa9500b06a445f1c88f6223f7f446dc1d19
Dionard <186435445+dionard2002@users.noreply.github.com>
2024-11-19 22:51:20 +0800

    initialized new dashboard

---
+++ CS26FinalProject/src/main/java/com/mycompany/cs26finalproject/MainDashboard.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.cs26finalproject;
+import javax.swing.JFrame;
+/**
+ *
+ * @author dionardvale
+ */
+public class MainDashboard {
+    public static void main (String[] args) {
+        
+    }
+}
